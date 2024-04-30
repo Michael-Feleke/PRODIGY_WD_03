4c7e3645a7ee5aa317b5fa2f987fae6ab0a56fe1
Michael Feleke <felekemichael999@gmail.com>
2024-05-01 01:13:04 +0300

    Refactoring script.js and final touch

--- script.js
+++ script.js
@@ -17,6 +17,17 @@ const cells = document.querySelectorAll(".cell");
 const status = document.getElementById("status");
 const resetBtn = document.getElementById("resetBtn");
 
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function startAIGame() {
   isAI = true;
   modeSelection.style.display = "none";
